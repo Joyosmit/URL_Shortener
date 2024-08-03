37b0dd1f94f196cc78834404ecc5473eaf498f7f
Joyosmit <joyosmitp@gmail.com>
2024-08-03 20:45:04 +0530

    Added basic frontend

---
+++ client/src/app/page.tsx
@@ -0,0 +1,58 @@
+'use client'
+import { useState } from "react";
+
+export default function Home() {
+    const [url, setUrl] = useState<string>('');
+    const handleSubmit = async (event:any) => {
+        event.preventDefault();
+        try {
+          const response = await fetch('http://localhost:8001/generate', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ originalUrl:url }),
+          });
+    
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+    
+          // Handle successful response
+          console.log('URL submitted successfully');
+        } catch (error) {
+          // Handle error
+          console.error('Error submitting URL:', error);
+        }
+      };
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <form
+                onSubmit={handleSubmit}
+                className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md"
+            >
+                <h2 className="text-xl font-semibold mb-4">Submit a URL</h2>
+                <div className="mb-4">
+                    <label htmlFor="url" className="block text-gray-700 mb-2">
+                        URL
+                    </label>
+                    <input
+                        type="url"
+                        id="url"
+                        name="url"
+                        value={url}
+                        onChange={(e) => setUrl(e.target.value)}
+                        required
+                        className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                </div>
+                <button
+                    type="submit"
+                    className="w-full bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition duration-200"
+                >
+                    Submit
+                </button>
+            </form>
+        </main>
+    );
+}
--- server/index.js
+++ server/index.js
@@ -3,12 +3,13 @@ require("dotenv").config();
 const router = require('./route/routes');
 
 const connect = require('./utils/connect');
+const cors = require('cors');
 connect();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-
+app.use(cors());
 app.use(express.json());
 app.use("/generate", router)
 
